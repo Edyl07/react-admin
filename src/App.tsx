579643230dab234d014c4c683a19df63048ce895
Demba <demba94diagana@gmail.com>
2021-01-22 01:09:26 +0100

    component template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './secure/components/Nav';
+import Dashboard from './secure/Dashboard';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Users from './secure/Users';
+import Login from './public/Login';
+import Register from './public/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+ <div className="App">
+    <div className="container-fluid">
+        <BrowserRouter>
+            <Route path={'/'} exact component={Dashboard} />
+            <Route path={'/users'} component={Users} />
+            <Route path={'/login'} component={Login} />
+            <Route path={'/register'} component={Register} />
+        </BrowserRouter>
     </div>
-  );
+ </div>
+  
+   );
 }
 
 export default App;
